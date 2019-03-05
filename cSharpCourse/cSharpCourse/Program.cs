c9500532f526865930551acac090b797a3c4b0f6
Pawel Magdanski <pmagdanski@gmail.com>
2019-03-05 20:59:15 +0100

    commit

--- cSharpCourse/cSharpCourse/Program.cs
+++ cSharpCourse/cSharpCourse/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace cSharpCourse
 {
@@ -10,6 +6,7 @@ namespace cSharpCourse
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Witaj świecie");
         }
     }
 }
