ccc6444f367660240ebaafe7e8b4c19108867431
Pawel Magdanski <pmagdanski@gmail.com>
2019-03-13 19:26:24 +0100

    Next update

--- cSharpCourse/Student diary/Program.cs
+++ cSharpCourse/Student diary/Program.cs
@@ -13,39 +13,14 @@ namespace Student_diary
             diary.AddRating(8.5f); // f żeby kompilator wiedział, że przekazujemy float
             diary.AddRating(4.7f);
 
-            float avg = diary.CalculateAverage();
-            float max = diary.GiveMaxRating();
-            float min = diary.GiveMinRating();
-
-            for (; ;)
-            {
-                Console.WriteLine("Podaj ocenę z zakresu 1 do 10");
-                float rating;
-                bool result = float.TryParse(Console.ReadLine(), out rating); // float.TryParse - sprawdza czy parsowana wartość to liczba
-                // out rating jeśli sparsuje to wysyłą do rating ???
-                if (rating == 11)
-                {
-                    break;
-                }
-                if (result)
-                {
-                    if (rating > 0 && rating <= 10)
-                    {
-                        diary.AddRating(rating);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Niepoprawna liczba");
-                    }
-                    
-                }
-            }
-            Console.WriteLine("Średnia Twoich ocen to : " + diary.CalculateAverage());
-            Console.WriteLine("Maksymalna ocena to : " + diary.GiveMaxRating());
-            Console.WriteLine("Minimalna ocena to : " + diary.GiveMinRating());
-            Console.ReadKey();
-
             Diary diary2 = new Diary();
+            diary2.AddRating(8);
+
+            Diary diary3 = new Diary();
+            diary3.AddRating(8);
+
+            
+            int liczba = 2;
         }
     }
 }
--- cSharpCourse/Student diary/diary.cs
+++ cSharpCourse/Student diary/diary.cs
@@ -6,15 +6,27 @@ namespace Student_diary
 {
     class Diary
     {
+
+        public Diary()
+            {
+            ratings = new List<float>();
+            }               
+
         // stock (var's = cells)
-        List<float> ratings = new List<float>(); // float - liczby zmiennoprzecinkowe
-
-
-        // actions
+        List<float> ratings;
+       // actions
+       /// <summary>
+       /// Add rating between 1 - 10
+       /// </summary>
+       /// <param name="rating"></param>
         public void AddRating(float rating)
         {
             ratings.Add(rating);
         }
+        /// <summary>
+        /// Calculate ratings average
+        /// </summary>
+        /// <returns></returns>
         public float CalculateAverage()
         {
             float sum = 0, avg = 0;
@@ -28,10 +40,18 @@ namespace Student_diary
 
             return avg;
         }
+        /// <summary>
+        /// Return highest rating
+        /// </summary>
+        /// <returns></returns>
         public float GiveMaxRating()
         {
             return ratings.Max();
         }
+        /// <summary>
+        /// Return lowest rating
+        /// </summary>
+        /// <returns></returns>
         public float GiveMinRating()
         {
             return ratings.Min();
