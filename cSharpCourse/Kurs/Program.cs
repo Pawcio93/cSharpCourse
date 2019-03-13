71ff45498300076425493b5b15a92ace86720faa
Pawel Magdanski <pmagdanski@gmail.com>
2019-03-13 20:36:20 +0100

    update

---
+++ cSharpCourse/Kurs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kurs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5, b = 10;
+            Console.WriteLine(a + "+" + b + "=" + (a + b));
+            Console.WriteLine(a + "-" + b + "=" + (a - b));
+            Console.WriteLine(a + "/" + b + "=" + (a / (double)b)); // (double) rzutowanie
+            Console.WriteLine(a + "*" + b + "=" + (a * b));
+
+            Console.WriteLine(a + "%" + b + "=" + (a % b)); // wypisuje resztę
+            Console.WriteLine(a++); // postinkrementacja i preinkrementacja (dekrementacja)
+            Console.WriteLine(a);
+
+            a += 3; // a = a + 3 -= , /= , *= 
+            Console.WriteLine(a);
+
+
+            Console.ReadLine();
+        }
+    }
+}
